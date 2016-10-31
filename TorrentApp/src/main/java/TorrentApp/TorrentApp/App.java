feaf3f6a285a0c603317fc6ad6f7c163eb8859df
Loupimo <julolebulot@free.fr>
2016-11-01 00:21:49 +0100

    Plus d'arguments pris en compte à l'exec pour la BDD (description, auteur, serveur, dbName, user, password) + Acceptation des apostrophes dans les descriptions et autres

--- TorrentApp/src/main/java/TorrentApp/TorrentApp/App.java
+++ TorrentApp/src/main/java/TorrentApp/TorrentApp/App.java
@@ -1,18 +1,15 @@
 package TorrentApp.TorrentApp;
 
-
-/**
- * Hello world!
- *
- */
 public class App 
 {
 	public static void main(String[] args)
 	{
 		Parser parse;
+		int argsCount = 0;
 		if (args.length > 0)
 		{
 			parse = new Parser (args[0]);
+			argsCount = 1;
 		}
 		else
 		{
@@ -23,7 +20,8 @@ public class App
 			//String oo = "TorrentTest\\Game_Dev_Tycoon_v1.4.15_(2-click_run).torrent"; // Anounce-list présent: à gérer
 			//String oo = "TorrentTest\\batman-unlimited-mechs-vs-mutants-french-webrip-2016.torrent"; // 
 			//String oo = "TorrentTest\\David Morell - Les conjures de la pierre.epub.torrent";
-			String oo = "TorrentTest\\David Morrell-Portrait de l'assassin en artiste.epub.torrent";
+			//String oo = "TorrentTest\\David Morrell-Portrait de l'assassin en artiste.epub.torrent";
+			String oo = "TorrentTest\\Windows 10 (Français), anniversary update 10.0.14393 (x64).iso.torrent";
 			
 			parse = new Parser(oo);
 		}
@@ -36,7 +34,12 @@ public class App
 			TrackerCommunicator tc = new TrackerCommunicator((String) parse.getDicoCombinationFromString("announce", null), parse.getBencodedDicoInfo(), "-UT3490-n%a6c%15%9d%ae%fd%1c%3e%0fd%aa", "50755", 0, 0, 10000, "started", 1);
 			TrackerResponse tr = new TrackerResponse (tc.HTTPGet());
 			tr.createPeerList();*/
-			new BDDLinker ("localhost", "8000", "Symfony", "root", "root", parse);
+			String [] parameters = {"localhost", "", "Symfony", "root", "root", "Jul'o", "Je viens de l'appli"}; // Stores the default parameters for the BDD. In order: host, port, dbName, userName, password, author, description
+			for (int i = 0; i < args.length; i++, argsCount++)
+			{
+				parameters[i] = args[argsCount];
+			}
+			new BDDLinker (parameters[0], parameters[1], parameters[2], parameters[3], parameters[4], parameters[5], parameters[6], parse);
 			
 			/*Tree arbo = new Tree (parse);
 			arbo.createArbo();*/
--- TorrentApp/src/main/java/TorrentApp/TorrentApp/BDDLinker.java
+++ TorrentApp/src/main/java/TorrentApp/TorrentApp/BDDLinker.java
@@ -26,7 +26,9 @@ public class BDDLinker
 	protected String torrentName; // Name of the torrent
 	protected long realFileSize;  // The total size of torrent contents (in byte). Note: most of the time 1 byte = 1 octet
 	protected Parser parser;	  // The parser
-	protected String fileName;	  // Name of the file
+	protected String fileName;	  // Name of file
+	protected String author; 	  // The author of torrent
+	protected String description; // A description of torrent
 	private static final Logger LOG = Logger.getLogger(BDDLinker.class);
 	
 	
@@ -34,7 +36,7 @@ public class BDDLinker
 	// Constructor //
 	/////////////////
 	
-	BDDLinker (String p_serverName, String p_portNumber, String p_dbName, String p_user, String p_password, Parser p_parser)
+	BDDLinker (String p_serverName, String p_portNumber, String p_dbName, String p_user, String p_password, String p_author, String p_description, Parser p_parser)
 	{
 		this.serverName = p_serverName;
 		this.portNumber = p_portNumber;
@@ -42,22 +44,10 @@ public class BDDLinker
 		this.user = p_user;
 		this.password = p_password;
 		this.parser = p_parser;
+		this.author = p_author;
+		this.description = p_description;
 		runSetters();
 		connectToBDD();
-	}
-	
-	
-	
-	BDDLinker (String p_serverName, String p_portNumber, String p_dbName, String p_user, String p_password, Parser p_parser, String p_torrentName, long p_realFileSize)
-	{
-		this.serverName = p_serverName;
-		this.portNumber = p_portNumber;
-		this.dbName = p_dbName;
-		this.user = p_user;
-		this.password = p_password;
-		this.parser = p_parser;
-		this.torrentName = p_torrentName;
-		this.realFileSize = p_realFileSize;
 	}
 	
 	
@@ -70,6 +60,7 @@ public class BDDLinker
 		
 		setTorrentName();
 		setRealFileSize();
+		adaptSpecialChars();
 	}
 	
 	
@@ -77,17 +68,13 @@ public class BDDLinker
 	protected void setTorrentName ()
 	{ // Gets the name of the torrent and set the variable
 		
-		torrentName = parser.getDicoCombinationFromString("name", null); // Retrives the name inside the .torrent
+		torrentName = parser.getDicoCombinationFromString("name", null); // Retrieves the name inside the .torrent
 		fileName = parser.torrentFile.getName();
 		
-		if (torrentName == null)
+		if (torrentName.equals(""))
 		{ // As "name" is an optional parameter if it doesn't exist we use the torrent file name
 			torrentName = parser.torrentFile.getName().replace(".torrent", "");
 		}
-		
-		torrentName = torrentName.replace("'", "\\\'");
-		fileName = fileName.replace("'", "\\\'");
-		System.out.println(torrentName);
 	}
 	
 	
@@ -111,10 +98,22 @@ public class BDDLinker
 		}
 		else
 		{ // There's only one file in the torrent
-			realFileSize = Integer.parseInt((String)parser.getDicoCombinationFromString("length", null)); // Retrives the file size inside the .torrent
+			realFileSize = Long.parseLong((String)parser.getDicoCombinationFromString("length", null)); // Retrives the file size inside the .torrent
 		}
 		
 		LOG.debug(" = " + realFileSize + " bytes");
+	}
+	
+	
+	
+	protected void adaptSpecialChars ()
+	{ // Re-write all ' to be understood in the SQL request
+		
+		torrentName = torrentName.replace("'", "\\\'");
+		fileName = fileName.replace("'", "\\\'");
+		author = author.replace("'", "\\\'");
+		description = description.replace("'", "\\\'");
+		System.out.println("torrentName = " + torrentName + "\nfileName = " + fileName + "\nauthor = " + author + "\ndescription = " + description);
 	}
 	
 	
@@ -132,10 +131,15 @@ public class BDDLinker
 		    connectionProps.setProperty("user", this.user);
 		    connectionProps.setProperty("password", this.password);
 		    connectionProps.setProperty("useSSL", "false");
+		    connectionProps.setProperty("useUnicode", "true");
+		    connectionProps.setProperty("characterEncoding", "UTF-8");
 		    
 		    /* Connection to the database */
 		    System.out.println("Try to connect to database");
-		    conn = (Connection) DriverManager.getConnection("jdbc:mysql://" + this.serverName /*+ ":" + this.portNumber */+ "/" + this.dbName, connectionProps);
+		    String connectionString = "jdbc:mysql://" + this.serverName;
+		    if (!this.portNumber.equals("")) connectionString += ":" + this.portNumber;
+		    connectionString +=  "/" + this.dbName;
+		    conn = (Connection) DriverManager.getConnection(connectionString, connectionProps);
 		    System.out.println("Connected to database");
 		    
 		    insertRowInDB(conn);
@@ -170,7 +174,7 @@ public class BDDLinker
 			Statement statement = conn.createStatement();
 			
 			// Insert the data
-			statement.executeUpdate("INSERT INTO Advert (NomTorrent, TailleFichier, date, auteur, description, Path_To_File) " + "VALUES ( '" + this.torrentName + "', " + this.realFileSize + ", '" + (new java.sql.Timestamp(Calendar.getInstance().getTimeInMillis()).toString()) + "', 'root', 'Je viens de l appli :p', '../uploads/torrent/" + this.fileName + "')");
+			statement.executeUpdate("INSERT INTO Advert (NomTorrent, TailleFichier, date, auteur, description, Path_To_File) " + "VALUES ( '" + this.torrentName + "', " + this.realFileSize + ", '" + (new java.sql.Timestamp(Calendar.getInstance().getTimeInMillis()).toString()) + "', '" + this.author + "', '" + this.description + "', '../uploads/torrent/" + this.fileName + "')");
 			
 			LOG.debug("Added an entry to the database");
 		}
